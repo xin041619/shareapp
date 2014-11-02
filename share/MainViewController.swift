0297e03ce8c3b1195b7ae6e94adf1089209f5420
xinxin <xinxin.wang.1@gmail.com>
2014-11-02 15:33:27 -0800

    first commit

---
+++ share/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  share
+//
+//  Created by xinxin wang on 10/19/14.
+//  Copyright (c) 2014 com.xinxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // unwind back from the next controller
+    @IBAction func unwindList(segue: UIStoryboardSegue) {
+        
+        
+    }
+}
+
---
+++ share/ShareInputFieldCell.swift
@@ -0,0 +1,22 @@
+//
+//  ShareInputFieldCell.swift
+//  share
+//
+//  Created by xinxin wang on 11/2/14.
+//  Copyright (c) 2014 com.xinxin. All rights reserved.
+//
+
+import UIKit
+
+class ShareInputFieldCell: UITableViewCell {
+
+    var labelText: String?
+    
+    func load(labelText:String) {
+        self.labelText = labelText
+    }
+    
+    
+//    @IBOutlet weak var optionLabel: UILabel!
+//    @IBOutlet weak var playIcon: UILabel!
+}
